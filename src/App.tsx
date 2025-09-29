0b359b4791b7fc72422013c2d169b3786a591170
yuntasha <jaljayo85@gmail.com>
2025-09-29 22:16:46 +0900

    feat: 컨텐츠 페이지 넣기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import Header from "./components/Header";
 import styles from "./App.module.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TimerPage from "./pages/timer/TimerPage";
+import TodoPage from "./pages/todo/TodoPage";
+import OptionPage from "./pages/option/OptionPage";
+import AuthPage from "./pages/login/AuthPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <div className={styles.container}>
         <Header />
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/" element={<TimerPage />} />
+            <Route path="/todo" element={<TodoPage />} />
+            <Route path="/option" element={<OptionPage />} />
+            <Route path="/login" element={<AuthPage />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   )
